7f8f85aebdce02877abbb899fd6bda4c76b954d1
Estudiante UIB <estudiante iub>
2026-09-11 08:56:22 -0500

    implementa el menu principal para la gestion de contactos

--- pro.py
+++ pro.py
@@ -3,3 +3,26 @@ telefono = []
 email = []
 direccion = []
 
+while True:
+    menu = """
+    ### ELIGUE UNA OPCION
+
+    1.Agregar un contacto
+    2. Buscar un contacto
+    3. Eliminar un contacto
+    4. Salir
+    """
+    opcion_elegida =int(input(menu))
+
+    if opcion_elegida == 1:
+     pass 
+    elif opcion_elegida == 2:
+     pass
+    elif opcion_elegida == 3:
+     pass
+    elif opcion_elegida == 4:
+     print("Hasta la vista baby")
+     break
+    else:
+        print("opcion invalida intenta nuevamente")
+
